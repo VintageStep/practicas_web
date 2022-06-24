b319f2c35905aea59b4938f11de13a8ba52f946f
Thomas <thomassilva90@gmail.com>
2022-06-24 14:45:58 +0200

    limpiar index.php y trabajo en hoja de estilo

--- index.php
+++ index.php
@@ -9,20 +9,20 @@
 <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
         <title>Login</title>
 </head>
 
 <body>
-        <div class="container">
-                <form name="formulario" method="post" action="/inicio_sesion_practica.php" class="login">
-                        <h1 class="formulario__titulo">Inicio sesión</h1>
+        <main class="container">
+                <form name="formulario" method="POST" action="/inicio_sesion_practica.php" class="login">
+                        <h1 class="login__titulo">Inicio de sesión</h1>
 
-                                <div class="mb-3">
+                                <div class="usuario__container">
                                         <label for="usuario" class="usuario__etiqueta">Usuario</label>
                                         <input type="text" name="usuario" class="login__usuario" id="usuario" placeholder="Ingrese email" required>
                                 </div>
-                                <div class="mb-3">
+                                <div class="clave__container">
                                         <label for="clave" class="clave__etiqueta">Contraseña</label>
                                         <input type="password" class="login__clave" name="clave" id="clave" placeholder="Ingrese contraseña" required><br>
                                         <a href="#" class="login__enlace">¿Olvidó contraseña?</a>
@@ -30,7 +30,7 @@
                                         <input type="submit" id="boton_ingreso" name="boton_ingreso" value="Ingresar" class="login__boton">
                         <p class="formulario__error"><?php $mensaje ?></p>
                 </form>
-        </div>
+        </main>
 </body>
 
 </html>
