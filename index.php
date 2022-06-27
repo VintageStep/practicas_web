5eb249088e719a0e9515ef3bf987c2701c760c7d
Thomas <thomassilva90@gmail.com>
2022-06-27 11:40:14 +0200

    login terminado

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Radio+Canada:wght@300;400;700&display=swap" rel="stylesheet">        
         <title>Login</title>
 </head>
 <body>
@@ -18,16 +21,19 @@
                         <h1 class="login__titulo">Inicio de sesión</h1>
                         <div class="login__contenedor">
                                 <div class="login__grupo">
-                                        <label for="usuario" class="usuario__etiqueta">Usuario</label>
-                                        <input type="text" name="usuario" class="login__usuario" id="usuario" placeholder="Email:" required>
+                                        <input type="text" name="usuario" class="login__input" id="usuario" placeholder=" " required>
+                                        <label for="usuario" class="login__label">Usuario</label>
                                 </div>
                                 <div class="login__grupo">
-                                        <label for="clave" class="clave__etiqueta">Contraseña</label>
-                                        <input type="password" class="login__clave" name="clave" id="clave" placeholder="Contraseña:" required>
+                                        <input type="password" class="login__input" name="clave" id="clave" placeholder=" " required>
+                                        <label for="clave" class="login__label">Contraseña</label>
                                 </div>
-                                        <a href="#" class="login__enlace">¿Olvidó contraseña?</a>
                                         <input type="submit" id="boton_ingreso" name="boton_ingreso" value="Ingresar" class="login__boton">
-                                <p class="formulario__error"><?php $mensaje ?></p>
+                                <p class="login__parrafo">
+                                        <a href="#" class="login__enlace">¿Olvidó su contraseña?</a>
+                                        <a href="#" class="login__enlace">Sobre nosotros</a>
+                                </p>        
+                                <p class="login__error"><?php $mensaje ?></p>
                         </div>
         </main>
 </body>
