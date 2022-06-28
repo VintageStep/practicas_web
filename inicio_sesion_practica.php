5783371ce29823e6504d8b31690c5620f8f9e8da
Maria <maria@gmail.com>
2022-06-28 11:56:04 +0200

    pruebas

--- inicio_sesion_practica.php
+++ inicio_sesion_practica.php
@@ -13,57 +13,70 @@ if(isset($_POST['usuario']) and isset($_POST['clave'])){
 // CONECTAR BBDD
 include 'crud\conexion_practica.php';
 
-// COMPROBAR SI EXISTE EL USUARIO EN LA BD
-$sql = "SELECT * FROM usuarios WHERE email_usuario = '$nombre_usuario'"; 
-$resultado = mysqli_query($conn,$sql);
-$fila = mysqli_fetch_row($resultado);
-if($fila[4]<>0) { // Si existe ese usuario, buscar su contraseña en la BD 
+// COMPROBAR SI EXISTE EL USUARIO EN LA BD Y LA CONTRASEÑA COINCIDEN
+$conn= Conectar();
+//$sql = "SELECT * FROM usuarios WHERE email_usuario = '$nombre_usuario' and clave_usuario= '$clave_usuario'"; 
+$resultado = $conn -> query("SELECT * FROM usuarios WHERE email_usuario = '$nombre_usuario' and clave_usuario= '$clave_usuario'");
+$fila= mysqli::mysqli_fetch_row($resultado);
+$usuario = $resultado->mysqli::fetchAll(PDO::FETCH_OBJ);
+$tipo= $usuario[0];
+
+if($tipo != NULL) {   // Si existe ese usuario y coincide con la contraseña devuelve el tipo de usuario con el Id_Tipo_usuario
     
-    $sql = "SELECT clave_usuario, id_tipo_usuario, nombre_usuario, pregunta_seguridad_usuario FROM usuarios WHERE email_usuario = '$nombre_usuario'"; 
-    $resultado = mysqli_query($conn,$sql);
+    if($usuario[0] == 1)
+    header('Location: http:/Perfiles/portada.php');
+
+    if($usuario[0] == 2)
+    header('Location: http:/Perfiles/portada.php');
+
+    if($usuario[0] == 3)
+    header('Location: http:/Perfiles/portada.php');
+}
+
+    //$sql = "SELECT clave_usuario, id_tipo_usuario, nombre_usuario, pregunta_seguridad_usuario FROM usuarios WHERE email_usuario = '$nombre_usuario'"; 
+    //$resultado = mysqli_query($conn,$sql);
     // mysqli_close($conn);
 
-    echo "<br>";
-    var_dump($resultado);
+    //echo "<br>";
+    //var_dump($resultado);
 
-    echo "<br>";
-    $fila = mysqli_fetch_assoc($resultado);   
-    var_dump($fila);
+    //echo "<br>";
+    //$fila = mysqli_fetch_assoc($resultado);   
+    //var_dump($fila);
 
-    $_SESSION["usuario"] = $fila['nombre_usuario'];
-    $keyword_bd = $fila['clave_usuario'];
+    //$_SESSION["usuario"] = $fila['nombre_usuario'];
+    //$keyword_bd = $fila['clave_usuario'];
 
     // COMPARAR CONTRASEÑAS
-    if ($clave_usuario == $keyword_bd){ // Coinciden las contraseñas
+    //if ($clave_usuario == $keyword_bd){ // Coinciden las contraseñas
 
         // Extraer el nombre y asignárselo a la variable $_SESSION["usuario"]
 
-        if(isset($_GET['usuario'])){
+        //if(isset($_GET['usuario'])){
 
-            $usuario = $_GET['usuario'];
+            //$usuario = $_GET['usuario'];
                 // Buscar en BD el nombre del usuario
-            include 'conexion_practica.php';
-            $sql = "SELECT nombre_usuario FROM usuarios WHERE email_usuario = '$usuario'"; 
-            $resultado = mysqli_query($conn,$sql);
-            $fila = mysqli_fetch_row($resultado);
+            //include 'conexion_practica.php';
+            //$sql = "SELECT nombre_usuario FROM usuarios WHERE email_usuario = '$usuario'"; 
+            //$resultado = mysqli_query($conn,$sql);
+            //$fila = mysqli_fetch_row($resultado);
             // $nombre = $fila[0]; // $nombre es el nombre (sin apellidos) del usuario
-            $nombre = $fila[0];    
+            //$nombre = $fila[0];    
         
-        }  
+        //}  
 
-        header('Location: http:/menu_opciones.php');
+        //header('Location: http:/menu_opciones.php');
 
-    } else { // Las contraseñas no coinciden  
+    //} else { // Las contraseñas no coinciden  
 
-        header("Location: http:/inicio.php?mensaje=1");
+        //header("Location: http:/inicio.php?mensaje=1");
         
-    } 
+    //} 
 
-} else { // El usuario no existe en la base de datos. Pasar un mensaje de erro a 'error.php'
-    //echo "No existe ese mail";
+else { // El usuario no existe en la base de datos. Pasar un mensaje de erro a 'error.php'
+    echo "No existe ese usuario";
     // header("Location: http:/error.php?mensaje=2");
-    header("Location: http:/inicio.php?mensaje=2");
+    //header("Location: http:/inicio.php?mensaje=2");
     
 }
-
 ?>
