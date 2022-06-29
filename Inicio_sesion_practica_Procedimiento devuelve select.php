29b6eb8fec56d308995804dbede279b0aaac7325
Maria <maria@gmail.com>
2022-06-29 11:47:34 +0200

    creacion de plantillas para bases de datos

--- Inicio_sesion_practica_Procedimiento devuelve select.php
+++ Inicio_sesion_practica_Procedimiento devuelve select.php
@@ -11,22 +11,29 @@ if(isset($_POST['usuario']) and isset($_POST['clave'])){
 include 'crud\conexionSQL.php';
 
 $conn= Conectar();
+
 $sql='Practicas.dbo.login_id_tipo ?,?';
 
 
 $email_usuario = 'maria@hola.com';
-$clave_usuario='1234';
-$resultado =sqlsrv_prepare( $conn,$sql,array($email_usuario,$clave_usuario));
+$clave_usuario='123';
+
+$resultado =sqlsrv_prepare( $conn,$sql,array(&$email_usuario,&$clave_usuario));
 
 $result = sqlsrv_execute($resultado);
 
+
+
 if( $result === false)  
 {  
-     echo "Eres gilipollas te has confundido tolai\n";  
+    echo "Eres gilipollas te has confundido tolai\n";  
      die( print_r( sqlsrv_errors(), true));  
 }  
 while( $row = sqlsrv_fetch_array( $resultado,SQLSRV_FETCH_NUMERIC))  
 {  
     $id_usuario=$row[0];
+    if ($id_usuario=='0'){
+    exit;
+    }
     $id_tipo_usuario=$row[1];
 } 
